81e2938eefe644cde0e3862660af564d2f7105c1
Caleb <cjr214@gmail.com>
2020-08-01 21:24:20 -0400

    Adding local storage

--- library/script.js
+++ library/script.js
@@ -28,24 +28,21 @@ function addBookToLibrary(event) {
     const author = document.getElementById("author").value;
     const title = document.getElementById("title").value;
     const pages = document.getElementById("pages").value;
-    const hasRead = document.getElementById("hasRead").value;
+    const hasRead = document.getElementById("hasRead").checked;
 
-    const book = new Book(author, title, pages, hasRead);
-    console.log(book);
+    const book = new Book(title, author, pages, hasRead);
     myLibrary.push(book);
-    //localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 
-    //removeForm();
     render();
 }
 
 function render() {
-    //myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
     const display = document.getElementById("books");
     display.innerHTML = "";
     myLibrary.forEach((book, idx) => {
         const card = document.createElement("div");
-
         display.innerHTML += `<div class="book-details">
                                 <div class="title">${book.title}</div>
                                 <div class="author">by ${book.author}</div>
@@ -68,12 +65,13 @@ function removeBook(idx) {
     const secondPart = myLibrary.slice(idx + 1, myLibrary.length);
 
     myLibrary = firstPart.concat(secondPart);
-    //localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
     render();
 }
 
 function changeReadStatus(idx) {
     myLibrary[idx].hasRead = !myLibrary[idx].hasRead;
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
     render();
 }
 
